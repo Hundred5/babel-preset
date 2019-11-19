35b368c737678c3fd3e64020b2be6c168309da49
Michal Miškerník <michal@miskernik.sk>
2019-11-19 17:04:13 +0200

    Basic Babel config

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = api => {
+  const test = api.env("test");
+
+  return {
+    presets: [
+      "@babel/preset-typescript",
+      "@babel/preset-react",
+      [
+        "@babel/preset-env",
+        {
+          targets: test ? { node: "current" } : undefined,
+          modules: test ? "commonjs" : false,
+          useBuiltIns: "usage",
+          corejs: 3
+        }
+      ]
+    ],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-syntax-dynamic-import"
+    ],
+    env: {
+      test: {
+        plugins: [["dynamic-import-node", { noInterop: true }]]
+      }
+    }
+  };
+};
